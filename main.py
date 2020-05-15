34125fd4177d5396c6a8af6182517a0608a47286
Aman <patilaman080@gmail.com>
2020-05-15 20:21:20 +0530

    added zip function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+
 def crateifnotexists(folder):
     if not os.path.exists(folder):
         print(f"creating {folder}")
@@ -10,34 +11,43 @@ def move(foldername, files):
     for file in files:
         os.replace(file,f"{foldername}/{file}")
 
-files=os.listdir()
-files.remove("main.py")
-print(files)
-crateifnotexists('images')
-crateifnotexists('docs')
-crateifnotexists('media')
-crateifnotexists('others')
+if __name__ =="__main__":
+    files=os.listdir()
+    files.remove("main.py")
+    print(files)
+    crateifnotexists('images')
+    crateifnotexists('docs')
+    crateifnotexists('media')
+    crateifnotexists('others')
+    crateifnotexists('zips')
 
-imgext=[".png",".jpg","jpeg",".gif"]
-images=[file for file in files if os.path.splitext(file)[1].lower() in imgext]
+    imgext=[".png",".jpg","jpeg",".gif"]
+    images=[file for file in files if os.path.splitext(file)[1].lower() in imgext]
 
-docext=[".txt",".pdf",".docx",".doc"]
-docs=[file for file in files if os.path.splitext(file)[1].lower() in docext]
+    docext=[".txt",".pdf",".docx",".doc",".xlsx",".csv"]
+    docs=[file for file in files if os.path.splitext(file)[1].lower() in docext]
 
-mediaext=[".mp3",".mp4",".mp5"]
-medias=[file for file in files if os.path.splitext(file)[1].lower in mediaext]
+    mediaext=[".mp3",".mp4",".mp5"]
+    medias=[file for file in files if os.path.splitext(file)[1].lower() in mediaext]
 
-#print(images)
-#print(docs)
-#print(medias)
-other=[]
-for file in files:
-    ext = os.path.splitext(file)[1].lower()
-    if(ext not in mediaext) and(ext not in docext) and(ext not in imgext) and os.path.isfile(file):
-        other.append(file)
+    zipext=[".zip",".exe",".rar"]
+    zipp=[file for file in files if os.path.splitext(file)[1].lower() in zipext]
 
-print(other)
-move("images",images)
-move("docs",docs)
-move("media",medias)
-move("others",other)
+
+    #print(images)
+    #print(docs)
+    #print(medias)
+    print(zipp)
+
+    other=[]
+    for file in files:
+        ext = os.path.splitext(file)[1].lower()
+        if(ext not in mediaext) and(ext not in docext) and (ext not in imgext) and (ext not in zipext)  and os.path.isfile(file):
+            other.append(file)
+
+    print(other)
+    move("images",images)
+    move("docs",docs)
+    move("media",medias)
+    move("others",other)
+    move("zips",zipp)
